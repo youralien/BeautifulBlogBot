403252e92fa8ae87c64ed04c54005ff5e1511456
youralien <ryan.louie@students.olin.edu>
2015-06-02 19:09:27 -0400

    FIX: authIds should not be committed!

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var path = require("path");
 
 var shutterstock = require('shutterstock');
  
-var api = shutterstock.v2({
-  clientId: 'c51e33f8d5ae3a980191',
-  clientSecret: '229b8cd8777e86f7a05c0cd2ab76c3a5b524f03c',
-});
+var api = shutterstock.v2(require("./shutterStockAuth"));
 
 /* DEFINE ROUTE CALLBACKS */
 var routes = {};
