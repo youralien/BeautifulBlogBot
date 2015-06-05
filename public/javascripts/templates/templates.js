9724959478bd975b888c094f4b8aec88bb2b5eb9
youralien <ryan.louie@students.olin.edu>
2015-06-04 23:03:27 -0400

    ADD: materialize for designs, new partials

--- public/javascripts/templates/templates.js
+++ public/javascripts/templates/templates.js
@@ -6,14 +6,20 @@ templates['photo'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(
   return "<li class='photo'>\n	<div>\n		<img class='centered' src=\""
     + alias3(((helper = (helper = helpers.photoUrl || (depth0 != null ? depth0.photoUrl : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"photoUrl","hash":{},"data":data}) : helper)))
     + "\">\n	</div>\n	<div>\n		<input class='centered' value=\""
+    + alias3(((helper = (helper = helpers.ownername || (depth0 != null ? depth0.ownername : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"ownername","hash":{},"data":data}) : helper)))
+    + "\">\n	</div>\n	<div>\n		<input class='centered' value=\""
     + alias3(((helper = (helper = helpers.photoUrl || (depth0 != null ? depth0.photoUrl : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"photoUrl","hash":{},"data":data}) : helper)))
     + "\">\n	</div>\n</li>";
 },"useData":true});
-templates['topTopics'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
+templates['topTopics'] = template({"1":function(depth0,helpers,partials,data) {
+    return "			<li class=\"collection-item\">"
+    + this.escapeExpression(this.lambda(depth0, depth0))
+    + "</li>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
 
-  return "<div>\n	<div>\n		<label for=\"topTopics\">Bot thinks your text is about </label>\n	</div>\n	<div>\n		<input id=\"topTopics\" value=\""
-    + this.escapeExpression(((helper = (helper = helpers.topTopics || (depth0 != null ? depth0.topTopics : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"topTopics","hash":{},"data":data}) : helper)))
-    + "\">\n	</div>\n	<div>\n		Searching...\n	</div>\n</div>";
+  return "<div>\n	<ul class=\"collection with-header\">\n		<li class=\"collection-header\"><h4>Article Topics</h4></li>\n"
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.topTopics : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
+    + "	</ul>\n</div>";
 },"useData":true});
 })();
