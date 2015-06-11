d230685509280661245a26e56d44fefcb6e99041
youralien <ryan.louie@students.olin.edu>
2015-06-11 13:12:42 -0400

    ADD: flickrOptions eek

---
+++ flickrOptions.js
@@ -0,0 +1,9 @@
+flickrOptions = {
+	api_key: "7a32e65b66332146f698fa623e26be55",
+	secret: "27946e7f8897db51",
+	user_id: "133753624@N06",
+	access_token: "72157653896708846-534cb6f5b4493495",
+	access_token_secret: "bd69ac818b225394"
+};
+
+module.exports = flickrOptions;
